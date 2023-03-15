5d7182fe503762f4d61f2c0a162beed9e5d5133a
Bohdan <bogdan.magometa@gmail.com>
2023-03-15 16:54:13 +0200

    Better model

--- modeling.py
+++ modeling.py
@@ -20,16 +20,18 @@ def calc_rmse(x1, x2):
 def main():
     train_df = pd.read_csv(TRAIN_DATASET_PATH)
 
+    # Train on train dataset and print achieved RMSE
     y = train_df['target']
-    x = train_df['6']**2
-    x = sm.add_constant(x)
+    train_df['53'] = train_df['6']**2
+    x = sm.add_constant(train_df[['7', '53']])
     model = sm.OLS(y, x).fit()
     train_rmse = calc_rmse(x @ model.params, y)
-    print(f"RMSE on train data: {train_rmse:.2f}")
+    print(f"RMSE on train data: {train_rmse:.3g}")
 
+    # Predicton on internship_hidden_test.csv
     test_df = pd.read_csv(TEST_DATASET_PATH)
-    x_test = test_df['6']**2
-    x_test = sm.add_constant(x_test)
+    test_df['53'] = test_df['6']**2
+    x_test = sm.add_constant(test_df[['7', '53']])
     predicted_target = x_test @ model.params
     predicted_target = pd.DataFrame({'target': predicted_target})
     predicted_target.to_csv(TARGET_OUTPUT_PATH, index=False)
